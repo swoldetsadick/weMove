0ac21f911d762d7d55bad2d6fe15fb3cae80b83e
swoldetsadick <kebrab67@gmail.com>
2017-01-22 19:39:42 +0100

    Building skeleton

---
+++ src/app/news/news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class NewsComponent {
+  title = 'app works!';
+}
