1a248402e2eeceb2ee9a6280f2a9621fac065785
swoldetsadick <kebrab67@gmail.com>
2017-01-24 00:28:37 +0100

    finished building skeleton & started with news component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  width: number = 250;
-  height: number = 250;
+    width: number = 250;
+    height: number = 250;
 
-  ngOnInit() {
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-  }
+    ngOnInit() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+    }
 
-  onResize(event) {
-    this.width = event.target.innerWidth;
-    this.height = event.target.innerHeight;
-  }
+    onResize(event) {
+      this.width = event.target.innerWidth;
+      this.height = event.target.innerHeight;
+      location.reload();
+    }
 }
