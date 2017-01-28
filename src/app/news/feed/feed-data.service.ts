8d057db956d39f5917f40f56e344de4b28575f64
Woldetsadick Selam Getachew <Selam-Getachew.Woldetsadick@bmw.de>
2017-01-28 10:20:33 +0100

    prepare for animation

--- src/app/news/feed/feed-data.service.ts
+++ src/app/news/feed/feed-data.service.ts
@@ -11,32 +11,12 @@ export class NewsDataService {
   constructor(private http: Http) {}
 
   getNewsFeeds(newsType: string){
-    let site: string = 'http://feeds.reuters.com/Reuters/' + newsType;
+    let site: string = 'http://feeds.reuters.com/' + newsType;
     let url: string = 'https://api.rss2json.com/v1/api.json?rss_url=' + site;
     // Service is refreshed every 10 minutes
     return Observable.timer(0, 600000).flatMap(() => { return this.http.get(url).map(response => <NewsFeed[]>response.json().items)});
 	}
 }
 
-//Arts	http://feeds.reuters.com/news/artsculture
-//Business	http://feeds.reuters.com/reuters/businessNews
-//Company News	http://feeds.reuters.com/reuters/companyNews
-//Entertainment	http://feeds.reuters.com/reuters/entertainment
-//Environment	http://feeds.reuters.com/reuters/environment
-//Health News	http://feeds.reuters.com/reuters/healthNews
-//Lifestyle	http://feeds.reuters.com/reuters/lifestyle
-//Media	http://feeds.reuters.com/news/reutersmedia
-//Money	http://feeds.reuters.com/news/wealth
-//Most Read Articles	http://feeds.reuters.com/reuters/MostRead
-//Oddly Enough	http://feeds.reuters.com/reuters/oddlyEnoughNews
-//Pictures	http://feeds.reuters.com/ReutersPictures
-//People	http://feeds.reuters.com/reuters/peopleNews
-//Politics	http://feeds.reuters.com/Reuters/PoliticsNews
-//Science	http://feeds.reuters.com/reuters/scienceNews
-//Sports	http://feeds.reuters.com/reuters/sportsNews
-//Technology	http://feeds.reuters.com/reuters/technologyNews
-//Top News	http://feeds.reuters.com/reuters/topNews
-//US News	http://feeds.reuters.com/Reuters/domesticNews
-//World	http://feeds.reuters.com/Reuters/worldNews
 // Integrate annyang then command are col1 and col2 parameter to annyang
 // First do the news banner and the table
--- src/app/news/feed/feed.component.ts
+++ src/app/news/feed/feed.component.ts
@@ -1,37 +1,103 @@
-import { Component} from '@angular/core';
+import { Component, trigger, state, style, transition, animate } from '@angular/core';
 import { NewsDataService } from './feed-data.service';
 import { NewsFeed } from './newsFeed';
 
 @Component({
   selector: 'feeds',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.css'],
+  animations: [
+    trigger('flyInOut', [
+        state('in', style({transform: 'translateY(0)'})),
+        transition('void => in', [
+            style({transform: 'translateY(-1000%)'}),
+            animate(10000)
+        ]),
+        transition('in => void', [
+            animate(10000, style({transform: 'translateY(1000%)'}))
+        ])
+    ])
+]
 })
+
 export class FeedComponent {
 
   newsfeeds: NewsFeed[];
-
-  constructor(private newsDataService: NewsDataService){}
-
+  myWidth: number;
   newsType: string;
 
+  constructor(private newsDataService: NewsDataService){
+
+  }
 
   ngOnInit(){
-    this.newsType = 'worldNews';
+    this.newsType = channels[Channels.Technology];
     this.newsDataService.getNewsFeeds(this.newsType).subscribe(data => {this.newsfeeds = data});
+  }
+
+  ngAfterContentChecked() {
+   if(this.newsfeeds != undefined) {
+     this.myWidth = 100 * this.newsfeeds.length;
+   }
   }
 
   changeChannel()//pass parameter in annyang
   {
-    if(this.newsType === 'technologyNews')
+    if(this.newsType === channels[Channels.Technology])
     {
-      this.newsType = 'worldNews';
+      this.newsType = channels[Channels.Entertainment];
       this.newsDataService.getNewsFeeds(this.newsType).subscribe(data => {this.newsfeeds = data});
     }
     else
     {
-      this.newsType = 'technologyNews';
+      this.newsType = channels[Channels.Technology];
       this.newsDataService.getNewsFeeds(this.newsType).subscribe(data => {this.newsfeeds = data});
     }
   }
 }
+
+enum Channels {
+  Arts,
+  Business,
+  CompanyNews,
+  Entertainment,
+  Environment,
+  HealthNews,
+  Lifestyle,
+  Media,
+  Money,
+  MostReadArticles,
+  OddlyEnough,
+  Pictures,
+  People,
+  Politics,
+  Science,
+  Sports,
+  Technology,
+  TopNews,
+  USNews,
+  World
+}
+
+let channels : Array<string> = [
+  "news/artsculture",
+  "reuters/businessNews",
+  "reuters/companyNews",
+  "reuters/entertainment",
+  "reuters/environment",
+  "reuters/healthNews",
+  "reuters/lifestyle",
+  "news/reutersmedia",
+  "news/wealth",
+  "reuters/MostRead",
+  "reuters/oddlyEnoughNews",
+  "ReutersPictures",
+  "reuters/peopleNews",
+  "Reuters/PoliticsNews",
+  "reuters/scienceNews",
+  "reuters/sportsNews",
+  "reuters/technologyNews",
+  "reuters/topNews",
+  "Reuters/domesticNews",
+  "Reuters/worldNews"
+];
